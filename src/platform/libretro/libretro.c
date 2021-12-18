b8ef99a291dd697b519e12faee59f17983468121
Ryunam <mt.ryunam@gmail.com>
2021-12-18 13:55:03 +0100

    Update core version string

--- src/platform/libretro/libretro.c
+++ src/platform/libretro/libretro.c
@@ -1329,7 +1329,7 @@ void retro_get_system_info(struct retro_system_info* info) {
 #ifndef GIT_VERSION
 #define GIT_VERSION ""
 #endif
-	info->library_version = "0.9.2" GIT_VERSION;
+	info->library_version = "0.9.3" GIT_VERSION;
 	info->library_name = "mGBA";
 	info->block_extract = false;
 }
